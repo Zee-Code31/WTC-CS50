4307f942cca51602d0af12978d3162215fc732ba
Zee-Code31 <sincaduz@gmail.com>
2025-08-24 21:53:35 +0200

    Add files via upload

---
+++ interpreter.py
@@ -0,0 +1,21 @@
+# implement a program that prompts the user for an arithmetic expression and then calculates
+# and outputs the result as a floating-point value formatted to one decimal place.
+# Assume that the user’s input will be formatted as x y z,
+# with one space between x and y and one space between y and z, wherein:
+# x is an integer
+# y is +, -, *, or /
+# z is an integer
+# For instance, if the user inputs 1 + 1, your program should output 2.0.
+# Assume that, if y is /, then z will not be 0.
+expression = input("Expression: " )
+x_ , y, z_ = expression.split(" ")
+x = float(x_)
+z = float(z_)
+if y== "+":
+    print(x+z)
+elif y== "-":
+    print(x-z)
+elif y== "*":
+    print(x*z)
+elif y== "/":
+    print(x/z)
